27c1407ac7967844245d18a74d76331842f87b50
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-04-20 13:46:00 -0300

    style - implementation of styles for the created components

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import './App.css';
 
 import Header from "./components/Header";
+import SocialMedia from "./components/SocialMedia";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <SocialMedia />
+      <Home />
     </>
   );
 }
