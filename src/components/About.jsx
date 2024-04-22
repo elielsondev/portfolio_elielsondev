5da7d495294314d907cb93702e41053a5a2e2286
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-04-22 15:23:11 -0300

    feat - implementation of the About component

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="aboutSection">
+      <article id="aboutArticle">
+        <h2 id="aboutTitle">Sobre</h2>
+        <hr />
+        <p id="aboutParagraph">
+          Meu nome é Elielson Nascimento (Eli) e estou em transição de carreira para
+          desenvolvimento de software - front-end. Sou comunicativo, disposto a
+          aprender constantemente, tenho facilidade em trabalhar em equipe e amo
+          o que faço. Estou determinado a me tornar um profissional de destaque
+          nessa área, contribuindo com paixão, habilidades e entusiasmo para
+          projetos inovadores. Estou confiante de que posso alcançar o sucesso e
+          estou preparado para abraçar essa nova fase da minha carreira.
+        </p>
+      </article>
+    </section>
+  );
+}
+
+export default About;
