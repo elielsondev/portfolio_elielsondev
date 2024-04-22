4bf0feb1a279dc1f00afeefda0bc4dbf0186fae7
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-04-22 15:21:38 -0300

    delete - removing elements that changed background color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,47 @@
-import React from 'react';
-import meuAvatar from '../utils/meuAvatar.png';
-import brasil from '../utils/Flag_of_Brazil.png';
-import unitedKingdon from '../utils/Flag_of_the_United_Kingdom.png';
+import React from "react";
+import meuAvatar from "../utils/meuAvatar.png";
+import brasil from "../utils/Flag_of_Brazil.png";
+import unitedKingdon from "../utils/Flag_of_the_United_Kingdom.png";
 
 // Documentação do pacote: https://hamburger-react.netlify.app/
-import { Divide as Hamburger } from 'hamburger-react'
+import { Divide as Hamburger } from "hamburger-react";
 
 function Header() {
   return (
-    <header id='header'>
-        <div id='logoAndTitle'>
-          <img id='logo' src={meuAvatar} alt="Meu Avatar" />
-          <h1 id='titleHeader'>Elielson Nascimento</h1>
+    <header id="header">
+      <div id="logoAndTitle">
+        <img id="logo" src={meuAvatar} alt="Meu Avatar" />
+        <h1 id="titleHeader">Elielson Nascimento</h1>
+      </div>
+
+      <div id="divLanguage">
+        <div>
+          <img
+            id="portuguese"
+            src={brasil}
+            alt="Portuguese [Bandeira do Brasil]"
+          />
+          <img
+            id="english"
+            src={unitedKingdon}
+            alt="English [Flag of the United Kingdon]"
+          />
         </div>
-        
-        <div id='divLanguage'>
-          <div>
-            <img id='portuguese' src={brasil} alt="Portuguese [Bandeira do Brasil]" />
-            <img id='english' src={unitedKingdon} alt="English [Flag of the United Kingdon]" />
-          </div>
-          
-          <div id='divColorBody'>
-            <div id='bodyDark'></div>
-            <div id='bodyLight'></div>
-          </div>
-          
-            <div id='sandwichMenu'>
-              <Hamburger onToggle={toggled => {
-                if (toggled) {
-                  alert('open') // open a menu
-                } else {
-                  alert('closed') // close a menu
-                }
-              }} />
-            </div>
+
+        <div id="sandwichMenu">
+          <Hamburger
+            onToggle={(toggled) => {
+              if (toggled) {
+                alert("open"); // open a menu
+              } else {
+                alert("closed"); // close a menu
+              }
+            }}
+          />
         </div>
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;
