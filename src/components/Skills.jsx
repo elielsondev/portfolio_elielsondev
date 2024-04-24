c64423f941c4e0a81fc34645127f6077ac509967
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-04-24 13:51:29 -0300

    feat - Creation of the Skills component, already implemented using props

---
+++ src/components/Skills.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Skills({ dataSelect }) {
+  return (
+    <section>
+        <article>
+            <h2>{ dataSelect.skillsTitle }</h2>
+            <hr />
+        </article>
+    </section>
+  )
+}
+
+export default Skills;
