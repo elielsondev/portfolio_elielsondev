a0303c956f36a510383067f550caade4edb2f2dd
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-04-24 16:13:06 -0300

    feat - implementation of skills in the component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React from "react";
 
 function Skills({ dataSelect }) {
   return (
-    <section>
-        <article>
-            <h2>{ dataSelect.skillsTitle }</h2>
-            <hr />
-        </article>
+    <section id="skillsSection">
+      <article id="skillsArticle">
+        <h2 id="skillsTitle">{dataSelect.skillsTitle}</h2>
+        <hr />
+        <p className="skillsParagraph">Soft-Skills</p>
+        <div id="softSkills">
+          {dataSelect.softSkills.map((skill, i) => {
+            return (
+              <span className="skillsCardSoft" key={i}>
+                {skill}
+              </span>
+            );
+          })}
+        </div>
+
+        <p className="skillsParagraph">Hard-Skills</p>
+        <div id="hardSkills">
+          {dataSelect.hardSkills.map(({ id, skill, image }) => {
+            return (
+              <span className="skillsCardHardSpan" key={id}>
+                <img className="skillsCardHard" src={image} alt={skill} />
+                <p className="skillName">{skill}</p>
+              </span>
+            );
+          })}
+        </div>
+      </article>
     </section>
-  )
+  );
 }
 
 export default Skills;
