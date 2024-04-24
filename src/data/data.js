07f3cfabb2f0b8c9e554fc1616e734e9db55ec25
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-04-24 13:48:05 -0300

    feat - Adding objects with hard skills and soft skills to the data.js document

--- src/data/data.js
+++ src/data/data.js
@@ -12,6 +12,107 @@ const data = [
     Além disso, possuo conhecimento na área de back-end e banco de dados,
     o que me permite desenvolver soluções completas e integradas para
     atender às necessidades dos clientes.`,
+    skillsTitle: "Habilidades",
+    hardSkills: [
+      {
+        id: 1,
+        skill: "HTML",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg",
+      },
+      {
+        id: 2,
+        skill: "CSS",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg",
+      },
+      {
+        id: 3,
+        skill: "JavaScript",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+      },
+      {
+        id: 4,
+        skill: "React",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+      },
+      {
+        id: 5,
+        skill: "TypeScript",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg",
+      },
+      {
+        id: 6,
+        skill: "Node.js",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg",
+      },
+      {
+        id: 7,
+        skill: "Jest",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
+      },
+      {
+        id: 8,
+        skill: "Redux",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+      },
+      {
+        id: 9,
+        skill: "Bootstrap",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain-wordmark.svg",
+      },
+      {
+        id: 10,
+        skill: "MySQL",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        id: 11,
+        skill: "Express",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg",
+      },
+      {
+        id: 12,
+        skill: "mongoDB",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-",
+      },
+      {
+        id: 13,
+        skill: "Python",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg",
+      },
+      {
+        id: 12,
+        skill: "Git",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg",
+      },
+      {
+        id: 13,
+        skill: "GitHub",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg",
+      },
+    ],
+    softSkills: [
+      "Proatividade",
+      "Comunicação",
+      "Organização",
+      "Empatia",
+      "Trabalho em Equipe",
+      "Planejamento",
+    ],
   },
   {
     id: 2,
@@ -26,6 +127,107 @@ const data = [
     Furthermore, I have knowledge in the back-end and database areas,
     which allows me to develop complete and integrated solutions for
     meet customer needs.`,
+    skillsTitle: "Skills",
+    hardSkills: [
+      {
+        id: 1,
+        skill: "HTML",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg",
+      },
+      {
+        id: 2,
+        skill: "CSS",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg",
+      },
+      {
+        id: 3,
+        skill: "JavaScript",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+      },
+      {
+        id: 4,
+        skill: "React",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+      },
+      {
+        id: 5,
+        skill: "TypeScript",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg",
+      },
+      {
+        id: 6,
+        skill: "Node.js",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg",
+      },
+      {
+        id: 7,
+        skill: "Jest",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
+      },
+      {
+        id: 8,
+        skill: "Redux",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+      },
+      {
+        id: 9,
+        skill: "Bootstrap",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain-wordmark.svg",
+      },
+      {
+        id: 10,
+        skill: "MySQL",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        id: 11,
+        skill: "Express",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg",
+      },
+      {
+        id: 12,
+        skill: "mongoDB",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-",
+      },
+      {
+        id: 13,
+        skill: "Python",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg",
+      },
+      {
+        id: 12,
+        skill: "Git",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg",
+      },
+      {
+        id: 13,
+        skill: "GitHub",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg",
+      },
+    ],
+    softSkills: [
+      "Proactivity",
+      "Communication",
+      "Organization",
+      "Empathy",
+      "Teamwork",
+      "Planning",
+    ],
   },
 ];
 
